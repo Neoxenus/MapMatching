73bba931755cb5c5bf81e00c57fd5122961b2758
Volodymyr Kyryliuk <kirilyukvolodimir2003@gmail.com>
2025-05-25 18:42:24 +0300

    inital commit

---
+++ MapMatchingProject/src/main/java/com/example/mapmatchingproject/MapMatchingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mapmatchingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapMatchingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapMatchingProjectApplication.class, args);
+    }
+
+}
